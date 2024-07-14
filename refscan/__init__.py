b4810feb613f985f8a69f6decf41eb749065d2d3
eecavanna <eecavanna@users.noreply.github.com>
2024-07-13 18:08:31 -0700

    Increase fault tolerance of `get_package_metadata` function

--- refscan/__init__.py
+++ refscan/__init__.py
@@ -1,11 +1,18 @@
-import importlib.metadata
+from importlib.metadata import metadata, PackageMetadata, PackageNotFoundError
 
 
 def get_package_metadata(key: str) -> str:
     r"""
     Returns metadata about the installed package.
 
-    Reference: https://github.com/python-poetry/poetry/issues/273#issuecomment-570999678
+    References:
+    - https://docs.python.org/3/library/importlib.metadata.html#distribution-metadata
+    - https://github.com/python-poetry/poetry/issues/273#issuecomment-570999678
     """
-    package_metadata = importlib.metadata.metadata("refscan")
-    return package_metadata[key] if key in package_metadata else ""
+    metadata_value = ""
+    try:
+        package_metadata: PackageMetadata = metadata("refscan")
+        metadata_value = package_metadata.get(key)
+    except PackageNotFoundError:
+        pass
+    return metadata_value
