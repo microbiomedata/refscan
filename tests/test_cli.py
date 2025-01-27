6f0297a5b412ac4cc2ec8496e8f947ff7c8ea45a
eecavanna <eecavanna@users.noreply.github.com>
2025-01-27 00:09:38 -0800

    Configure Rich to not colorize output when running via `CliRunner`

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -60,7 +60,19 @@ def test_graph_command_with_custom_output_path():
 
 def test_graph_command_help():
     """Test the `graph` command with the --help flag."""
-    runner = CliRunner()
+
+    # Note: For this test, we define an environment variable (in the context of the `CliRunner`) named `NO_COLOR`. When
+    #       Rich "sees" that environment variable, Rich will refrain from colorizing its output (you can define an
+    #       environment variable named `FORCE_COLOR` to influence Rich in the opposite way). The reason we influence
+    #       Rich in this way here is that, when we don't, this test fails when run via GitHub Actions. I think it has
+    #       to do with the fact that GitHub Actions Runners do not allocate a TTY.
+    #
+    # References:
+    # - https://click.palletsprojects.com/en/stable/api/#click.testing.CliRunner (re: the `env` kwarg)
+    # - https://rich.readthedocs.io/en/stable/console.html#environment-variables (re: `FORCE_COLOR` and `NO_COLOR`)
+    # - https://github.com/actions/runner/issues/241 (re: GHA Runners not allocating a TTY)
+    #
+    runner = CliRunner(env=dict(NO_COLOR=""))
     result = runner.invoke(app, ["graph", "--help"])
 
     assert result.exit_code == 0, f"Unexpected exit code: {result.exit_code}"
